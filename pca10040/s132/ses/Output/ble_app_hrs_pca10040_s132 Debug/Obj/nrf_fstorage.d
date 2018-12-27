e0f26f134aab134c3ae4b88f5b2254dcd3efafb2
Anatolii <gumby@ukr.net>
2018-12-27 19:50:59 +0200

    first commit

---
+++ pca10040/s132/ses/Output/ble_app_hrs_pca10040_s132 Debug/Obj/nrf_fstorage.d
@@ -0,0 +1,12 @@
+Output/ble_app_hrs_pca10040_s132\ Debug/Obj/nrf_fstorage.o: \
+ /home/anatolii/BLE/nRF5_SDK_14.2.0_17b948a/components/libraries/fstorage/nrf_fstorage.c \
+ ../config/sdk_config.h \
+ /home/anatolii/BLE/nRF5_SDK_14.2.0_17b948a/components/libraries/fstorage/nrf_fstorage.h \
+ /usr/share/segger_embedded_studio_for_arm_3.34a/include/stdint.h \
+ /usr/share/segger_embedded_studio_for_arm_3.34a/include/stdbool.h \
+ ../../../../../../components/libraries/util/sdk_errors.h \
+ ../../../../../../components/softdevice/s132/headers/nrf_error.h \
+ ../../../../../../components/libraries/experimental_section_vars/nrf_section.h \
+ ../../../../../../components/libraries/util/nordic_common.h \
+ /usr/share/segger_embedded_studio_for_arm_3.34a/include/stddef.h \
+ /usr/share/segger_embedded_studio_for_arm_3.34a/include/__crossworks.h
---
+++ pca10040/s132/ses/Output/ble_app_hrs_pca10040_s132 Debug/Obj/thumb_crt0.d
@@ -0,0 +1,2 @@
+Output/ble_app_hrs_pca10040_s132\ Debug/Obj/thumb_crt0.o: \
+ /usr/share/segger_embedded_studio_for_arm_3.34a/source/thumb_crt0.s
